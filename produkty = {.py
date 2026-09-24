df1bfb0d95adba18dcd977e6538ae7a207a36a29
Hyper-Squid <hypersniperfn@gmail.com>
2026-09-24 21:29:49 +0200

    domaca uloha/produkty plus sklad

--- produkty = {.py
+++ produkty = {.py
@@ -1,40 +1,43 @@
-produkty = {
-    "jablko": (0.5, "ovocie"),
-    "banan": (0.45, "ovocie"),
-    "hruska": (0.7, "ovocie"),
-    "marhula": (0.8, "ovocie"),
-    "slivka": (0.6, "ovocie"),
-    "mrkva": (0.4, "zelenina"),
-    "petrzlen": (0.5, "zelenina"),
-    "celer": (0.9, "zelenina"),
-    "zemiak": (0.3, "zelenina"),
-    "cokolada": (1.1, "sladkost"),
-    "cukor": (0.75, "sladkost"),
-    "mlieko": (1.0, "ina vec"),
-    "chlieb": (0.8, "ina vec")
-}
+produkty = ["jablko", "banan", "mrkva", "zemiak", "mlieko"]
+ceny = [0.50, 0.45, 0.30, 0.25, 1.20]
+druhy = ["ovocie", "ovocie", "zelenina", "zelenina", "mliečne"]
+sklad = [5, 3, 4, 6, 2]
 
-nakupny_kosik = [
-    "jablko",
-    "mlieko",
-    "cukor",
-    "cokolada",
-    "chlieb",
-    "banan"
-]
+kosik = []
+celkova_cena = 0
 
-pocet = int(input("Kolko veci chces pridat do kosika?\n"))
+while True:
+    print("\n--- OBCHOD ---")
 
-while pocet:
-    print("Co chcete pridat?")
-    nazov = input().strip().lower()
+    for i in range(len(produkty)):
+        if sklad[i] == 0:
+            print(produkty[i], "-", druhy[i], "-", ceny[i], "€ - VYPREDANÉ")
+        else:
+            print(produkty[i], "-", druhy[i], "-", ceny[i], "€ - sklad:", sklad[i])
 
-    if nazov in produkty:
-        nakupny_kosik.append(nazov)
-        pocet -= 1
+    vyber = input("\nNapíš názov produktu (alebo 'koniec'): ").lower()
+
+    if vyber == "koniec":
+        break
+
+    if vyber not in produkty:
+        print("Tento produkt neexistuje!")
+        continue
+
+    pozicia = produkty.index(vyber)
+
+    if sklad[pozicia] == 0:
+        print("Tento produkt je vypredaný!")
     else:
-        print("Tato polozka nie je v zozname.")
+        kosik.append(produkty[pozicia])
+        celkova_cena = celkova_cena + ceny[pozicia]
+        sklad[pozicia] = sklad[pozicia] - 1
 
-for polozka in nakupny_kosik:
-    cena, kategoria = produkty[polozka]
-    print(f"{polozka} - {cena} EUR - {kategoria}")
+        print("Produkt bol pridaný do košíka!")
+
+print("\n--- KOŠÍK ---")
+
+for produkt in kosik:
+    print("-", produkt)
+
+print("Celková cena:", celkova_cena, "€")
